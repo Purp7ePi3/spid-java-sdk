5ef87f6440cc0614713eeef4244bc61f4a18e1d7
purple <sbrunelli01@gmail.com>
2026-04-06 14:43:12 +0200

    feat: aggiunto SingleLogoutService e KeyDescriptor encryption in SpMetadataGenerator

--- spid-metadata/src/main/java/it/spid/metadata/SpMetadataGenerator.java
+++ spid-metadata/src/main/java/it/spid/metadata/SpMetadataGenerator.java
@@ -33,14 +33,38 @@ public class SpMetadataGenerator {
 
   public String build() throws Exception {
     if (certificate == null) {
-      throw new IllegalAccessError("Need certificate");
+      throw new IllegalStateException("Il certificato è obbligatorio per generare il metadata");
     }
     String certBase64 = Base64.getEncoder().encodeToString(certificate.getEncoded());
+
+    // SingleLogoutService è opzionale ma incluso se configurato
+    String sloBlock = "";
+    if (config.getSingleLogoutServiceUrl() != null && !config.getSingleLogoutServiceUrl().isBlank()) {
+      sloBlock = """
+              <md:SingleLogoutService
+                  Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
+                  Location="%s"/>
+          """.formatted(config.getSingleLogoutServiceUrl());
+    }
+
+    // Organization block opzionale
+    String orgBlock = "";
+    if (organizationName != null && organizationUrl != null) {
+      orgBlock = """
+            <md:Organization>
+              <md:OrganizationName xml:lang="it">%s</md:OrganizationName>
+              <md:OrganizationDisplayName xml:lang="it">%s</md:OrganizationDisplayName>
+              <md:OrganizationURL xml:lang="it">%s</md:OrganizationURL>
+            </md:Organization>
+          """.formatted(organizationName, organizationName, organizationUrl);
+    }
+
     return """
         <?xml version="1.0" encoding="UTF-8"?>
         <md:EntityDescriptor
             xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata"
             xmlns:ds="http://www.w3.org/2000/09/xmldsig#"
+            xmlns:xml="http://www.w3.org/XML/1998/namespace"
             entityID="%s">
           <md:SPSSODescriptor
               AuthnRequestsSigned="true"
@@ -53,17 +77,26 @@ public class SpMetadataGenerator {
                 </ds:X509Data>
               </ds:KeyInfo>
             </md:KeyDescriptor>
-            <md:AssertionConsumerService
+            <md:KeyDescriptor use="encryption">
+              <ds:KeyInfo>
+                <ds:X509Data>
+                  <ds:X509Certificate>%s</ds:X509Certificate>
+                </ds:X509Data>
+              </ds:KeyInfo>
+            </md:KeyDescriptor>
+        %s    <md:AssertionConsumerService
                 Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
                 Location="%s"
-                index="0"/>
+                index="0"
+                isDefault="true"/>
           </md:SPSSODescriptor>
-        </md:EntityDescriptor>
+        %s</md:EntityDescriptor>
         """.formatted(
         config.getEntityId(),
         certBase64,
-        config.getAssertionConsumerServiceUrl());
-
+        certBase64,
+        sloBlock,
+        config.getAssertionConsumerServiceUrl(),
+        orgBlock);
   }
-
 }
